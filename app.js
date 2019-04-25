e54c3746fd3a48f65a57f08013061c23db8c8335
da Vinci <brucewilson1103@gmail.com>
2019-04-25 09:01:30 -0400

    DOM manipulation

--- app.js
+++ app.js
@@ -4,12 +4,33 @@ new Vue({
     title: 'Becoming a Vue Ninja',
     name: 'Ryker',
     url: "http://www.youtube.com",
-    classes: ["one", "two"]
+    classes: ["one", "two"],
+    wage: 10,
+    coords: {
+      x:0,
+      y:0
+    }
   },
   
   methods: {
     greet(time){
       return `Live long and prosper on this good ${time} in your journey whilst ${this.title}, ${this.name}`
+    },
+    wageIncrease(){
+      this.wage++;
+    },
+    wageDecrease(){
+      this.wage--;
+    },
+    changeWage(amount){
+      this.wage += amount;
+    },
+    logEvent(e){
+      console.log(e);
+    },
+    logCoords(e){
+      this.coords.x =e.offsetX
+      this.coords.y =e.offsetY
     }
   }
 })
